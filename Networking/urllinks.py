0b113e0b74088e57b7ee6d12ed36184719b75088
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-26 04:49:46 -0800

    Add Comments to each line of code to explain

--- Networking/urllinks.py
+++ Networking/urllinks.py
@@ -6,9 +6,14 @@ from bs4 import BeautifulSoup
 
 url = input('Enter -')
 html = urllib.request.urlopen(url).read()
+
+#This line creates a BeautifulSoup object by passing in the html variable and the HTML parser that should be used.
 soup = BeautifulSoup(html, 'html.parser')
 
+#This line uses the soup object to find all a (anchor) tags in the HTML document. 
 tags = soup('a')
+
+#This line prints the value of the href attribute of each tag, followed by None. The get method is used to retrieve the value of the href attribute.
 for tag in tags:
     print(tag.get('href'), None)
     
