0fa3f2396b4fbd40c3aa26545fbd5a1c2b9fe36d
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-27 01:00:15 -0800

    Arithmetic operations

---
+++ Basics/numbers.py
@@ -0,0 +1,35 @@
+x = 28
+print(type(x))
+
+y = 3.14
+print(type(y))
+
+# convert interger to float
+# float(x)
+# print(type(x))
+
+# convert float to number
+# int(y)
+# print(type(y))
+
+#complex numbers
+z = 2 + 0j 
+print(type(z))
+
+#Addition : int plus interger is a float
+c= x + y 
+print(c)
+
+# Subtraction : int minus interger is a float
+d = x - y
+print(d)
+
+#Multiplication : int plus interger is a float
+e = x * y 
+print(e)
+
+# Division : int minus interger is a float. even without a remainder it wil return a float
+f = x / y 
+print(f)
+
+
