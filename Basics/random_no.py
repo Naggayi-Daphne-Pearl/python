b6569338b5d25a0bc4decb24ff68744421b7040e
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-27 09:15:26 -0800

    Uniform Random Numbers

--- Basics/random_no.py
+++ Basics/random_no.py
@@ -1,5 +1,9 @@
 #Generating random numbers not less than 0 but not greater than 1 i.e [0, 1)
 import random 
-
+#10 random numbers
 for i in range(10):
     print(random.random())
+
+# unform distributions 
+for i in range(10):
+    print(random.uniform(3,7))
