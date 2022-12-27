f2f995b57a6c16f946616b1d42bc8325352a5286
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-27 08:39:02 -0800

    Memoization to speed up the execution of the function to iterate up a range of 1001

--- Basics/fib.py
+++ Basics/fib.py
@@ -1,10 +1,22 @@
+# Computing values using Fibonacci sequences
+# Memoization:
+fibonnacci_cache = {}
 def fibonnacci(n):
     if n == 1:
-        return 1
+        value = 1
     elif n == 2:
-        return 2
+        value = 1
     elif n > 2:
-        return fibonnacci(n-1) + fibonnacci(n-2)
-        
-for n in range (1,11) :
+        value = fibonnacci(n-1) + fibonnacci(n-2)
+        # Cache the value and return it
+    fibonnacci_cache[n] = value 
+    return value 
+for n in range(1,1001):
     print(n, ":", fibonnacci(n))
+    
+
+# # This method slows down for long sequences.        
+# for n in range (1,11) :
+#     print(n, ":", fibonnacci(n))
+
+
