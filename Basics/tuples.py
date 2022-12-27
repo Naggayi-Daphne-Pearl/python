446d1c943ba19e54ff39f33942c7397df1184880
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-27 06:51:48 -0800

    Tuples: Immutable sequence

---
+++ Basics/tuples.py
@@ -0,0 +1,13 @@
+# Using the () operator
+my_tuple = (1, 2, 3, 4)
+print(my_tuple)  # Output: (1, 2, 3, 4)
+
+# Using the tuple() function
+my_tuple = tuple([1, 2, 3, 4])
+print(my_tuple)  # Output: (1, 2, 3, 4)
+
+# Access elements in a tuple 
+print(my_tuple[0]) # Output: 1
+
+print(type(my_tuple)) # Output:<class 'tuple'>
+
