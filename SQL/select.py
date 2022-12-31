6bbdd9be73187c5ce8c415174049567be9cc2945
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-31 02:42:23 -0800

    Select values from table in database

--- SQL/select.py
+++ SQL/select.py
@@ -4,16 +4,16 @@ import psycopg2
 conn = psycopg2.connect(
     host="localhost",
     port=5432,
-    user="user",
+    user="postgres",
     password="password",
-    database="socratica"
+    database="postgres"
 )
 
 # This line creates a cursor, which is an object that allows you to execute SQL queries and fetch the results
 cur = conn.cursor()
 
 #This line executes a SQL query to select all rows from the "chitter_user" table
-cur.execute("SELECT * FROM chitter_user")
+cur.execute("SELECT * FROM posts")
 
 #This line fetches all the rows returned by the SQL query and stores them in the table 
 results = cur.fetchall()
