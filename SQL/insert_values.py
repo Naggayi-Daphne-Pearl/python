5a5e0006f65a55f6d5fece0987b0a735291cb7ed
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-31 02:48:15 -0800

    Inserting values to the database using python

---
+++ SQL/insert_values.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+# Connect to the database
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    user="postgres",
+    password="password",
+    database="postgres"
+)
+
+# Create a cursor
+cur = conn.cursor()
+
+# Insert a row into the "posts" table using parameterized queries
+cur.execute("INSERT INTO posts (post_id, post_text, user_id) VALUES (%s, %s, %s)", (2, 'I am inserting values to the database using python', 1))
+
+# Commit the changes to the database
+conn.commit()
+
+# Close the cursor and connection
+cur.close()
+conn.close()
