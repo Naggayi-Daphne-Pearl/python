8597f2bad1ad6c6ff2c39fc12793aaab8ef9b5e1
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-01 02:58:51 -0800

    Classes in python

--- Basics/dictionary.py
+++ Basics/dictionary.py
@@ -1,21 +1,21 @@
 #social media post 
 
 post = {"user_id": 900, "title": "My first post", "lang": "en", "date": "27/09/2022"},
-print(type(post))
+print(type(post)) # Output {'user_id': 900, 'title': 'My second post', 'lang': 'en'}
 
 post2 = dict (user_id = 900, title = "My second post", lang = "en")
-print(post2)
+print(post2) # Output 
 
 post2["message"] ="I am learning about dictionary"
 post2["datetime"] = "27/12/2022"
-print(post2)
+print(post2) # Output {'user_id': 900, 'title': 'My second post', 'lang': 'en', 'message': 'I am learning about dictionary', 'datetime': '27/12/2022'}
 
 #Accessing values in dictionary
 print(post2['message'])
 
 # Accessing a value that isnt in dictionary
 loc = post2.get('location', None)
-print(loc)
+print(loc) #Output: None
 
 #Looping through values in dictionary
 
