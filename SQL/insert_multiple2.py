d3b05745bf8a7583426878c811009a1fe23dd035
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-01 01:58:25 -0800

    Append list for multiple inputs

--- SQL/insert_multiple2.py
+++ SQL/insert_multiple2.py
@@ -16,7 +16,7 @@ cur = conn.cursor()
 # Define a list of tuples containing the values to be inserted
 values = []
 for i in range(2, 1002):
-    values.append((i+1, f'user{i}', f'user{i}@example.com',))
+    values.append((i, f'user{i}', f'user{i}@example.com',))
 
 # Insert a row into the "posts" table using parameterized queries
 cur.executemany("INSERT INTO chitter_user (user_id, user_name, email) VALUES (%s, %s, %s)", values)
