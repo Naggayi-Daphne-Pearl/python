983846b2364a9d882efb312fe7b21e851fd95bbd
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-04 06:52:52 -0800

    Class methods

--- OOP/class_attributes.py
+++ OOP/class_attributes.py
@@ -9,10 +9,26 @@ class Person:
 
     def __init__(self, name):
         self.name = name
-        Person.number_of_people += 1
+        Person.add_person()
+
+    """- Class Methods: denoted with @classmethod
+        - They act on the class itself. Dont have a class instance
+        - cls references the class
+    """
+    @classmethod
+    def number_of_person(cls):
+        return cls.number_of_people()
+
+    @classmethod 
+    def add_person(cls):
+        cls.number_of_people += 1
+
 
 
 p1 = Person("tim")
 print(Person.number_of_people)
 p2 = Person("pearl")
 print(Person.number_of_people)
+
+# class method
+print(Person.number_of_person())
