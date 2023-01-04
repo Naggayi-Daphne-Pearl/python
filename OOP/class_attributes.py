633ceb0df59da7eed96ed0650e393b0c31baee79
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-04 06:29:10 -0800

    Defining class attribute

--- OOP/class_attributes.py
+++ OOP/class_attributes.py
@@ -1,16 +1,18 @@
-# class attributes 
-"""Class attributes are variables defined inside a class definition and associated with the class itself"""
+# class attributes
+
 
 class Person:
+    """Class attributes are variables defined inside a class definition and associated with the class itself
+    Class attribute can be used to create a constant value within a class eg setting a constant for gravity i.e Gravity = 9.8"""
     """This is a class attribute"""
     number_of_people = 0
-    def __init__(self,name):
+
+    def __init__(self, name):
         self.name = name
         Person.number_of_people += 1
+
 
 p1 = Person("tim")
 print(Person.number_of_people)
 p2 = Person("pearl")
 print(Person.number_of_people)
-
-
