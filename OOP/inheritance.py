2f00ebee25a6e30ddb64282c6c8763c5f9c9edec
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-04 04:12:38 -0800

    Defining a general class with shared functionalites for both the classes

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -1,14 +1,24 @@
-class Cat:
-    def __init__(self,name,age):
+# Inheritance: 
+class Pet: 
+    """Inheritance method: Pet Class contains the functionality shared by the class Cat and Dog.
+    Pet Class is a general class  """
+    def __init__(self, name,age): 
         self.name = name
         self.age = age
+    def show(self):
+        print(f"I am {self.name} and {self.age} years old")
+
+
+"""Class Cat and Dog have functions that are more specific and different from each other """
+class Cat(Pet):
     def speak(self):
         print('Meow')
 
-class Dog:
-    def __init__(self,name, age):
-        self.name = name
-        self.age = age
+class Dog(Pet):
     def speak(self):
         print('Bark!')
+
+p = Pet('Pearl', 4)
+p.show()
+
 
