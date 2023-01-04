db342dca3707ea7c86978f0e54821f0d83df12fd
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-04 05:14:15 -0800

    Creating another attribute in the child class

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -1,7 +1,7 @@
 # Inheritance: 
 class Pet: 
-    """Inheritance method: Pet Class contains the functionality shared by the class Cat and Dog.
-    Pet Class is a general class  """
+    """Inheritance method: Pet Class contains the functionality shared by the children classes
+    Pet Class is a parent class """
     def __init__(self, name,age): 
         self.name = name
         self.age = age
@@ -9,16 +9,28 @@ class Pet:
         print(f"I am {self.name} and {self.age} years old")
 
 
-"""Class Cat and Dog have functions that are more specific and different from each other """
+"""Class Cat and Dog have functions that are more specific and different from each other. 
+These are child classes """
 class Cat(Pet):
+    def __init__(self,name,age,color):
+        super().__init__(name,age)
+        self.color = color
     def speak(self):
         print('Meow')
+    def show(self):
+        print(f"I am {self.name} and {self.age} years old and I color {self.color}") 
+    
 
 class Dog(Pet):
     def speak(self):
         print('Bark!')
 
+# Intiante 
 p = Pet('Pearl', 4)
 p.show()
+c = Cat('Bill', 13, 'green')
+c.show()
+c.speak()
+
 
 
