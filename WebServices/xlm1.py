e4d4f1f1d89c480d6ba39e6327a2b482a837ad43
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-04 03:37:38 -0800

    XML in python

--- WebServices/xlm1.py
+++ WebServices/xlm1.py
@@ -17,6 +17,8 @@ stuff = ET.fromstring(input)
 lst = stuff.findall('users/user')
 print('User count:', len(lst))
 for item in lst:
+    #  The text attribute of the name and id elements is used to get the text content of these elements. The text and get() methods return strings, which are printed to the console.
     print('Name:', item.find('name').text)
     print('Id:', item.find('id').text)
+    #get() gets the attribute value 
     print('Attribute:', item.get('x'))
--- WebServices/xlm2.py
+++ WebServices/xlm2.py
@@ -2,6 +2,9 @@ import xml.etree.ElementTree as ET
 from inspect import getmembers, isclass, isfunction
 
 # Display classes in ET MODUE 
-tree = ET.parse('./holders.xml')
+tree = ET.parse('holders.xml')
 root = tree.getroot()
-print(ET.tostring(root))
+
+# Get attribute 
+coin = root.get('coin')
+print('Crypto name: {val}'. format(val=coin))
