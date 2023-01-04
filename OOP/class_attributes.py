e2f800c2a9154bf2fda473c17c940db63e5a81bc
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-04 06:26:35 -0800

    Creating a class attribute to find out how many instances have been made

--- OOP/class_attributes.py
+++ OOP/class_attributes.py
@@ -3,11 +3,14 @@
 
 class Person:
     """This is a class attribute"""
-    number_of_people = 8
+    number_of_people = 0
     def __init__(self,name):
         self.name = name
+        Person.number_of_people += 1
 
 p1 = Person("tim")
+print(Person.number_of_people)
 p2 = Person("pearl")
-Person.number_of_people = 4
-print(p2.number_of_people)
+print(Person.number_of_people)
+
+
